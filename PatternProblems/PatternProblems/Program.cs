4d50b0cef39f6cfd6d83ba8b2d442f9c9d5e5e76
Nandhini1709 <nandhinimahalingam261@gmail.com>
2023-02-04 15:21:21 +0530

    Squareboard pattern & Checkboard pattern are completed

---
+++ PatternProblems/PatternProblems/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintSquareBoardPattern(n));
+            Console.WriteLine(PrintCheckBoardPattern(n));
+        }
+        public static string PrintSquareBoardPattern(int n)
+        {
+            string temp = "";
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    temp += "#";
+                }
+                temp += "\n";
+            }
+            return temp;
+        }
+        public static string PrintCheckBoardPattern(int n)
+        {
+            string temp = "";
+            for (int rows = 1; rows <= n; rows++)
+            {
+                if (rows % 2 == 0)
+                {
+                    temp += " ";
+                }
+
+                for (int col = 1; col <= n; col++)
+                {
+                    temp += "#";
+                }
+
+                temp += "\n";
+            }
+            return temp;
+        }
+    }
+}
